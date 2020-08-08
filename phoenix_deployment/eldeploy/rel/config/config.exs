fc6fd3752b104cf6b127249b771eb7ad0a108712
Siddhant <ssiddhant3030@gmail.com>
2020-08-08 21:24:43 +0530

    new file for docker

--- phoenix_deployment/eldeploy/rel/config/config.exs
+++
@@ -1,5 +0,0 @@
-use Mix.Config
-
-config :eldeploy, EldeployWeb.Endpoint,
-       url: [host: System.get_env("APP_HOST"), port: 4000, scheme: "http"],
-       secret_key_base: System.get_env("SECRET_KEY_BASE")
