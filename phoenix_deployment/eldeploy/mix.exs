b356ee02fa7416aef40ebe6ab9fa593fb5c60730
Siddhant <ssiddhant3030@gmail.com>
2020-08-09 19:26:57 +0530

    dist removed

--- phoenix_deployment/eldeploy/mix.exs
+++ phoenix_deployment/eldeploy/mix.exs
@@ -42,8 +42,7 @@ defmodule Eldeploy.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.1"}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
