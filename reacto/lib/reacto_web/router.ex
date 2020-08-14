c79b3283d6cedf6bad0102fbae53ae966456ee06
siddhant3030 <ssiddhant3030@gmail.com>
2020-08-14 18:34:01 +0530

    fetch_live_flash added

--- reacto/lib/reacto_web/router.ex
+++ reacto/lib/reacto_web/router.ex
@@ -5,6 +5,7 @@ defmodule ReactoWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
+    plug :fetch_live_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
