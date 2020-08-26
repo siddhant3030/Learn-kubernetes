d0d58f70f25462ef6f9ee6f1f28af895b65dd369
siddhant3030 <ssiddhant3030@gmail.com>
2020-08-27 00:29:28 +0530

    docker compose command with build and without

--- visits/index.js
+++ visits/index.js
@@ -3,13 +3,14 @@ const redis = require('redis')
 
 const app = express();
 const client = redis.createClient({
-    host: 'redis-server'
+    host: 'redis-server',
+    port: 6379
 });
 client.set('visits', 0)
 
 
 app.get('/', (req, res) => {
-    client.send('visits', (err, visits) => {
+    client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits)
         client.set('visits', parseInt(visits) + 1)
     })
