573945b1078bc3675596fd8d58db76a514285306
siddhant3030 <ssiddhant3030@gmail.com>
2020-08-27 01:01:32 +0530

    restart policies

--- visits/index.js
+++ visits/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis')
+const process = require('process')
 
 const app = express();
 const client = redis.createClient({
@@ -10,6 +11,7 @@ client.set('visits', 0)
 
 
 app.get('/', (req, res) => {
+    process.exit(0);
     client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits)
         client.set('visits', parseInt(visits) + 1)
