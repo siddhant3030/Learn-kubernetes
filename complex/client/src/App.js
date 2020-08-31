052fcc65c49a041cf2adc1d2960d4a6968dae9f5
siddhant3030 <ssiddhant3030@gmail.com>
2020-08-31 19:10:37 +0530

    router added in the app

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import OtherPage from './OtherPage'
+import Fib from './Fib'
+
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Link to="/">Home</Link>
+        <Link to="/otherPage">Other Page</Link>
+       <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+       </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +28,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Router>
   );
 }
 
