91ddcd614160250deebb06858f8d894571e1d286
siddhant3030 <ssiddhant3030@gmail.com>
2020-08-31 15:21:46 +0530

    worke

--- complex/worker/index.js
+++ complex/worker/index.js
@@ -13,3 +13,9 @@ function fib(index) {
     if(index < 2) return 1
     return fib(index - 1) + fib(index - 2)
 }
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)))
+})
+
+sub.subscribe('insert');
