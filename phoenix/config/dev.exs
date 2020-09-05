13d86bca41d1b3c164e4081d3bebf4158eaa72d5
siddhant3030 <ssiddhant3030@gmail.com>
2020-09-05 12:31:45 +0530

    docker change

--- phoenix/config/dev.exs
+++ phoenix/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :phoenix, Phoenix.Repo,
   username: "postgres",
   password: "postgres",
-  database: "phoenix_dev",
+  database: "phoenix",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
