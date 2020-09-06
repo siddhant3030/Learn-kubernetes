a97ec4a472ca65b3664e5e2ce52383eaa7bc81aa
siddhant3030 <ssiddhant3030@gmail.com>
2020-09-07 02:25:21 +0530

    built a markdwon parse

--- parser/config/dev.exs
+++ parser/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :parser, Parser.Repo,
   username: "postgres",
   password: "postgres",
-  database: "parser_dev",
+  database: "parser",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
--- parser/lib/parser/parse.ex
+++ parser/lib/parser/parse.ex
@@ -1,3 +1,9 @@
 defmodule Parser.Parse do
+  def to_html(text) do
 
+  end
+
+  def italics(text) do
+    Regex.replace(~r/\*(.*)\*/, text, "<em>\\1</em>")
+  end
 end
