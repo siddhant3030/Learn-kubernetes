064757d6e0b974f30da8281451c2bc1a2471ebde
siddhant3030 <ssiddhant3030@gmail.com>
2020-09-08 09:21:39 +0530

    kubernetes work

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const os = require('os')
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send(`Hi from ${os.hostname()}`)
+    res.send(`Hello from ${os.hostname()}`)
 })
 
 const port = 3000
